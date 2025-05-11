8db3140b6477a8a3e8bb52386b939da03a339b76
muratovic58 <106527558+muratovic58@users.noreply.github.com>
2025-05-11 18:11:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # Configure Gemini API
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-model = genai.GenerativeModel('gemini-1.0-pro')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 app = Flask(__name__)
 
